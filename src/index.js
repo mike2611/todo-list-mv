200b5b0739e4c1a92e4acb2bfdb5c92f5fcf4917
mike2611 <mpuentes2611@gmail.com>
2021-09-22 10:52:18 -0500

    Add task by index

--- src/index.js
+++ src/index.js
@@ -11,14 +11,23 @@ class Task {
 }
 
 const task1 = new Task(0, 'Task 1');
-const task2 = new Task(1, 'Task 2');
-const task3 = new Task(2, 'Task 3');
+const task2 = new Task(2, 'Task 3');
+const task3 = new Task(1, 'Task 2');
 
 const tasks = [task1, task2, task3];
 const ul = document.getElementById('list');
 
 function populateTask(arr) {
-  arr.forEach((task) => {
+  const orderArray = [];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = 0; j < arr.length; j += 1) {
+      if (i === arr[j].index) {
+        orderArray.push(arr[j]);
+      }
+    }
+  }
+  orderArray.forEach((task) => {
     const li = document.createElement('li');
     li.innerHTML = `${task.description} <i class="bi bi-three-dots-vertical"></i>`;
     li.classList.add('tasks', 'd-flex-between');
@@ -26,4 +35,4 @@ function populateTask(arr) {
   });
 }
 
-populateTask(tasks);
+window.onload(populateTask(tasks));

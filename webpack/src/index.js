bb697432b2ccbc670b22ba37d272ec1f368e2980
mike2611 <mpuentes2611@gmail.com>
2021-09-21 21:30:01 -0500

    Add styles to CSS and Add icons

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
 
+require('bootstrap-icons/font/bootstrap-icons.css');
+
 class Task {
   constructor(ind = 0, des = '', comp = false) {
     this.index = ind;
@@ -8,9 +10,9 @@ class Task {
   }
 }
 
-const task1 = new Task(0, 'task 1');
-const task2 = new Task(1, 'task 2');
-const task3 = new Task(2, 'task 3');
+const task1 = new Task(0, 'Task 1');
+const task2 = new Task(1, 'Task 2');
+const task3 = new Task(2, 'Task 3');
 
 const tasks = [task1, task2, task3];
 const ul = document.getElementById('list');
@@ -18,7 +20,8 @@ const ul = document.getElementById('list');
 function populateTask(arr) {
   arr.forEach((task) => {
     const li = document.createElement('li');
-    li.innerHTML = `${task.description}`;
+    li.innerHTML = `${task.description} <i class="bi bi-three-dots-vertical"></i>`;
+    li.classList.add('tasks', 'd-flex-between');
     ul.appendChild(li);
   });
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -19,8 +19,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        include: path.resolve(__dirname, './node_modules/bootstrap-icons/icons'),
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'webfonts',
+            publicPath: '../webfonts',
+          },
+        },
       },
     ],
   },

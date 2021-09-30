1ea790b2574e0bf365060128113d75ad57eb7241
ThankGod Richard <madueketf@gmail.com>
2021-09-30 02:49:13 +0100

    Test add task

--- src/crud.test.js
+++ src/crud.test.js
@@ -10,12 +10,15 @@ class Task {
   }
 }
 
-test('Add new task', () => {
-  document.body.innerHTML = '<div>'
-  + '<input type="text" name="input-task" id="input-task" required maxlength="60">'
-  + '</div>';
-  document.querySelector('#input-task').value = 'task2';
-  const task1 = new Task(1, 'task1');
-  const task2 = new Task(2, 'task2');
-  expect(addTask()).toEqual([task1, task2]);
+describe('Testing add and delete method', () => {
+  test('Add 2 new task, Should return two task', () => {
+    document.body.innerHTML =
+      '<div>' +
+      '<input type="text" name="input-task" id="input-task" required maxlength="60">' +
+      '</div>';
+    document.querySelector('#input-task').value = 'task2';
+    const task1 = new Task(1, 'task1');
+    const task2 = new Task(2, 'task2');
+    expect(addTask()).toEqual([task1, task2]);
+  });
 });
